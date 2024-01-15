ce81c6ae1c8e9c3ebb536eb21b5796d2d5046ade
Litezy <liteb237@gmail.com>
2024-01-15 14:44:16 +0100

    headers

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -7,7 +7,7 @@ const PageLayout = ({ children }) => {
         <div className='grid md:grid-cols-1 lg:grid-cols-7'>   
             <div className="h-screen hidden lg:block lg:col-span-2"></div>
             <div className="h-screen bg-white lg:col-span-3">
-                <div className="h-[10vh] bg-blue-800 flex w-[95%] md:w-full">
+                <div className="h-[10vh] bg-blue-800 flex w-[100%] md:w-full">
                 <Header />
                 </div>
                 <div className="h-[85vh] overflow-y-auto">
--- src/components/SingleBlog.jsx
+++ src/components/SingleBlog.jsx
@@ -5,10 +5,10 @@ import ButtonComponent from './ButtonComponent'
 import profilepic from '../assets/images/cat.jpg'
 const SingleBlog = () => {
   return (
-    <div className='mb-10 '>
+    <div className='mb-10 bg-white/45'>
         <BlogUserComponent pic={profilepic} title={'Mike Ashley'} post={'Posted: 20 minutes ago'}/>
         <div className="mt-4 overflow-y-hidden">
-            <img src={blogimg} className=' w-full h-[15rem] object-cover' alt="blogimg" />
+            <img src={blogimg} className=' w-[95%] rounded-lg mx-auto h-[15rem] object-cover' alt="blogimg" />
             <div className="w-11/12 mx-auto px-1">
             <h1 className='font-bold text-2xl mt-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
             <p className='text-md font-md mt-3 text-orange-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit neque pariatur tempora est minima vitae iusto amet labore esse culpa atque aliquid nulla dolores nemo, deleniti ipsum accusantium! Ea, debitis.</p>
